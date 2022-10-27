6ececbf5a0df942ec4a8d4b8b6e29de01bb5cbe0
huskyHsu <schi.hsu@gmail.com>
2022-10-27 16:15:27 +0800

    fix: 🐛 update jwt to get user

--- server/src/articles/articles.module.ts
+++ server/src/articles/articles.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [ArticlesController],
   providers: [ArticlesService],
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule],
 })
 export class ArticlesModule {}
--- server/src/roles/roles.guard.ts
+++ server/src/roles/roles.guard.ts
@@ -1,15 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from 'src/enums/role.enum';
-import { UsersService } from 'src/users/users.service';
 import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    private usersService: UsersService,
-  ) {}
+  constructor(private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
@@ -19,11 +15,7 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const {
-      user: { userId },
-    } = context.switchToHttp().getRequest();
-
-    const user = await this.usersService.findById(userId);
+    const { user } = context.switchToHttp().getRequest();
 
     return requiredRoles.some((role) => user.roles?.includes(role));
   }
